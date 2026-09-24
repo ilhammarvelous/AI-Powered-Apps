50e9a71cd8b49b22a4806beda34eaa16d54c5197
ilham <kurniawanvamoz@gmail.com>
2026-09-24 13:35:20 +0700

    Setup Project(Next Js & Shadcn), Create Layout Dashboard UI, Data Structure & Vector Function, Setup Supabase Connection, Test Connection Supabase, Show List Data Transaction, Limit-pagination & Search, Insert Data(Manual), Delete Data(Manual), Update Data(Manual)

---
+++ src/lib/utils.ts
@@ -0,0 +1,17 @@
+// export { cn } from "cn"
+
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs));
+}
+
+export function convertToIDR(value: number) {
+    return new Intl.NumberFormat('id-ID', {
+        style: 'currency',
+        currency: 'IDR',
+        maximumFractionDigits: 0,
+    }).format(value);
+}
+
